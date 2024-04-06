5f24e531b149afa4d90f4b0af1c6cb2a15e27064
Ali Ergüç <ergucali1@gmail.com>
2024-04-07 01:53:51 +0300

    Add Annouchment Process
    Add Annouchment Processing.

--- announcements_frontend/src/components/common/DataGrid.js
+++ announcements_frontend/src/components/common/DataGrid.js
@@ -1,6 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { DataGrid } from '@mui/x-data-grid'
 import axios from 'axios';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 const columns = [
   { field: 'id', headerName: 'ID',width: 300 },
   { field: 'title', headerName: 'Title', width: 300 },
@@ -9,20 +18,65 @@ const columns = [
 ]
 
 
+
 const DataTable= () => {
   const [tableData, setTableData] = useState([])
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   useEffect(() => {
     axios.get('https://localhost:44324/api/announcements')
     .then(response =>  setTableData(response.data));
   }, [])
 
-  console.log(tableData)
+    const addAnnouchments = () => {
+      axios.post("https://localhost:44324/api/announcements", {
+        title: title,
+        content: content
+      })
+      .then((response) => {
+        console.log("EKLEME ISLEMI : "+JSON.stringify(response));
+      });
+    };
 
+    const handleTitleTextFieldChange = (event) => {
+      setTitle(event.target.value);
+    };
 
+    const handleContentTextFieldChange = (event) => {
+      setContent(event.target.value);
+    };
 
   return (
-    <div style={{ height: 700, width: '100%' }}>
+    <div style={{ height: 700, width: '100%' }}>   
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          Add Annouchement Settings
+        </AccordionSummary>
+        <AccordionDetails>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 12, sm: 12, md: 12 }}>
+          <Grid item xs={12}>
+          <TextField value={title} onChange={handleTitleTextFieldChange} label="Title" color="primary" focused />
+
+          </Grid>
+          <Grid item xs={12}>
+          <TextField value={content} onChange={handleContentTextFieldChange} label="Content" color="primary" focused />
+
+          </Grid>
+          <Grid item xs={12}>
+          <Button onClick={addAnnouchments} variant="outlined">Add Annouchment</Button>
+          </Grid>
+        </Grid>
+
+
+        </AccordionDetails>
+      </Accordion>
+   
       <DataGrid
         rows={tableData}
         columns={columns}
